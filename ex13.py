32b31c8d8873333a884dc89cdd3a1d6fb0fb4b15
Seraphine Young <seraphine_ene@yahoo.com>
2019-04-11 14:47:21 +0100

    completed exercise 13

---
+++ ex13.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+script, age, height, country = argv
+
+name = input("what is your name? ")
+status = input("Are you single? ")
+
+print(f"My name is {name}, I am {status}, my height is {height}, I am {age} years old and I am from {country}")
+
+#python3.7 ex13.py thirty 5 nigeria
+
+# script, first, second, third = argv
+# print("This script is called: ", script)
+# print("Your first variable is called: ", first)
+# print("Your second variable is called: ", second)
+# print("Your third variable is called: ", third)
+
+# #run this by python3.7 ex13.py stuff things that
